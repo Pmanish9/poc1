3779bbb2bb0bf475a225d2eed92969ebf1489074
Pmanish9 <mani.kumar724@gmail.com>
2023-04-04 21:10:00 +0530

    first commit

---
+++ app.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Umanish/poc-project1/handler"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	port := "9000"
+
+	log.Printf("Starting up on http://localhost:%s", port)
+
+	r := chi.NewRouter()
+
+	r.Get("/welcome", handler.Welcome)
+	r.Get("/timer", handler.Timer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte("Hello World!"))
+	})
+
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
